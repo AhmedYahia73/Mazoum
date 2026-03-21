a3c1dd43d61196cfb447bacbe7a4e8f53ec04aa3
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-03-21 22:27:56 +0200

    dfgdf

--- routes/user.php
+++ routes/user.php
@@ -5,22 +5,10 @@ use Illuminate\Support\Facades\Route;
 use Api\CustomEvent\ChatController;
 use Api\CustomEvent\PackageController;
 use Api\CustomEvent\UserController;
-
-Route::controller(ChatController::class)
-->prefix("chat")->group(function () {
-
-});
+ 
 
 Route::controller(PackageController::class)
 ->prefix("package")->group(function () {
     Route::get('/', 'view');  
 
-});
-
-Route::controller(UserController::class)
-->prefix("user")->group(function () {
-    Route::get('/', 'view');    
-    Route::get('/lists', 'lists');    
-    Route::post('/add', 'create');    
-
-});
+}); 

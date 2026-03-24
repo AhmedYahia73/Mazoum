224f775e215f51f7a292f866c421d837b3eb6440
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-03-25 01:24:24 +0200

    commit

--- app/Http/Controllers/EventUserActionsController.php
+++ app/Http/Controllers/EventUserActionsController.php
@@ -35,8 +35,8 @@ class EventUserActionsController extends Controller
         where('code', $code)
         ->with("event")
         ->firstOrFail();
-        $check_receive_apology = EventUserActions::where('event_user_id',$event_user->id)->where('action','refuse_event')->first();
-        $qr_row = $check_receive_apology ? null : Qr_Code::where('event_user_id',$event_user->id)->first();        
+        $check_receive_apology = EventUserActions::where('event_user_id',$event_user->id)->where('action','accept_event')->first();
+        $qr_row = $check_receive_apology ? Qr_Code::where('event_user_id',$event_user->id)->first() : null;        
         $accept_event = EventUserActions::
         where('event_user_id', $event_user->id)
         ->where('action','accept_event')

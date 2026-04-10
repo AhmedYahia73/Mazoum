bfd00a2ea1cddbba2fbc0d31f3bb97686d1c3cba
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-04-10 22:29:36 +0200

    dfgerd

--- config/database.php
+++ config/database.php
@@ -118,13 +118,11 @@ return [
     */
 
     'redis' => [
-
         'client' => env('REDIS_CLIENT', 'phpredis'),
-
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-        ],
+            'options' => [
+                'cluster' => env('REDIS_CLUSTER', 'redis'),
+                'prefix' => '', // اجعله نص فارغ لضمان تطابق اسم القناة
+            ],
 
         'default' => [
             'url' => env('REDIS_URL'),

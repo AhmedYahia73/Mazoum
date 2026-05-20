8f4c52430158178a7fc2f3716a93187dcbd839ec
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-05-21 02:57:03 +0300

    dsfgerd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,10 +22,11 @@ use App\Models\WattsChat as WattsChatModel;
 use App\Models\WebDesgins;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 use Intervention\Image\ImageManagerStatic as Image;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {

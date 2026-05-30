d93e4b7245231499680bdb7de5c305e063196098
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-05-30 23:41:49 +0300

    feat: add customPDF blade template for attendee response confirmation and apology links

--- resources/views/PDF/customPDF.blade.php
+++ resources/views/PDF/customPDF.blade.php
@@ -5,27 +5,59 @@
 <style>
 @page { size: A4 portrait; margin: 0; }
 body { margin: 0; padding: 0; }
-.wrap { position: relative; width: 210mm; height: 297mm; }
-.bg { position: absolute; top: 0; left: 0; width: 210mm; height: 297mm; }
-.btns { position: absolute; bottom: 25mm; left: 0; width: 100%; text-align: center; }
-.tbl { margin: 0 auto; }
-.td1 { padding-right: 6mm; }
-.td2 { padding-left: 6mm; }
-.a1 { display: block; padding-top: 4mm; padding-bottom: 4mm; padding-left: 8mm; padding-right: 8mm; font-size: 14pt; font-weight: bold; font-family: Arial; text-decoration: none; color: #ffffff; background-color: #1e6b40; }
-.a2 { display: block; padding-top: 4mm; padding-bottom: 4mm; padding-left: 8mm; padding-right: 8mm; font-size: 14pt; font-weight: bold; font-family: Arial; text-decoration: none; color: #ffffff; background-color: #8e2020; }
+.bg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 210mm;
+    height: 297mm;
+    z-index: -1;
+}
+.btns {
+    position: fixed;
+    bottom: 25mm;
+    left: 0;
+    width: 100%;
+    text-align: center;
+}
+.a1 {
+    display: inline;
+    padding-top: 3mm;
+    padding-bottom: 3mm;
+    padding-left: 7mm;
+    padding-right: 7mm;
+    font-size: 13pt;
+    font-weight: bold;
+    font-family: Arial;
+    text-decoration: none;
+    color: #ffffff;
+    background-color: #1e6b40;
+}
+.a2 {
+    display: inline;
+    padding-top: 3mm;
+    padding-bottom: 3mm;
+    padding-left: 7mm;
+    padding-right: 7mm;
+    font-size: 13pt;
+    font-weight: bold;
+    font-family: Arial;
+    text-decoration: none;
+    color: #ffffff;
+    background-color: #8e2020;
+}
+.spacer {
+    display: inline-block;
+    width: 10mm;
+}
 </style>
 </head>
 <body>
-<div class="wrap">
 <img src="{{ $imageBase64 }}" class="bg" />
 <div class="btns">
-<table class="tbl" cellpadding="0" cellspacing="0">
-<tr>
-<td class="td1"><a href="{{ $confirm_link }}" class="a1">تأكيد الحضور</a></td>
-<td class="td2"><a href="{{ $apologize_link }}" class="a2">الاعتذار عن الحضور</a></td>
-</tr>
-</table>
-</div>
+    <a href="{{ $confirm_link }}" class="a1">تأكيد الحضور</a>
+    <span class="spacer"></span>
+    <a href="{{ $apologize_link }}" class="a2">الاعتذار عن الحضور</a>
 </div>
 </body>
 </html>

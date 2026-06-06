79c8e94d4cfbe3a5de1d27c916107fcca4f2e5ea
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-06-07 01:34:49 +0300

    dsgdf

--- app/Http/Controllers/Admin/CustomEventController.php
+++ app/Http/Controllers/Admin/CustomEventController.php
@@ -1453,59 +1453,27 @@ class CustomEventController extends Controller
 
                 $template_name = 'car_msg3_';
 
-                if($request->sending_type2 == 'old_send') {
+                $caption = "ضيفتنـا الغاليـة , ننتظـرك يوم ". $date ." في تمــام الساعة "  . $time . "  تشرفينــا لحضور " . $request->message2 . ' 🌺🌺 ';
 
-                    $language = 'ar';
+                // $caption2 = 'تحرص الشركة على تقديم المساعدة للضيف حتى لا توجه اي صعوبات في دخول المناسبة تم ارسال الكود مره ثانية ,يرجى العلم ان الكود نفس الكود المرسل في السابق وليس كودا جديداً ';
 
-                    $token          = get_whats_setting($event)['token'];
-                    $sender_id      = get_whats_setting($event)['sender_id'];
-                    $phone_numer_id = get_whats_setting($event)['sender_id'];
+                // $api=$client->sendChatMessage($to,$body);
+                $api = $client->sendImageMessage($to,$url_image,$caption,$priority,$referenceId,$nocache);
+                $api2 = $client->sendLocationMessage($to,$event->address,$event->lat,$event->lng,$priority=0,$referenceId="SDK");
 
-                    $param_1 = $message;
-                    $param_2 = $time;
-                    $param_3 = $date;
+                // $qr_code_row = Qr_Code::where('event_user_id',$arr['id'])->latest()->first();
 
-                    $response = SendCarMsgTemplate($to,$template_name,$language,$url_image,$param_1,$param_2,$param_3,$phone_numer_id,$token);
+                // if($qr_code_row) {
+                //     $image_link = url('qr_code/' . $qr_code_row->qr);
+                //     // $api3 = $client->sendImageMessage($to,$image_link,$caption2,$priority,$referenceId,$nocache);
+                // }
 
-                    if ($response != null && $response->getStatusCode() == 200) {
-
-                        $body = $response->getBody();
-                        $data = json_decode($body, true);
-
-                    } else {
-                        $user_event->update([
-                            'status' => 'failed-v2',
-                        ]);
-                    }
-
+                if(! empty($api) && isset($api['sent']) && $api['sent'] == 'true'  && isset($api['message']) && $api['message'] == 'ok') {
+                    // dd('ok');
+                    info('error sending');
                 } else {
-
-                    $caption = "ضيفتنـا الغاليـة , ننتظـرك يوم ". $date ." في تمــام الساعة "  . $time . "  تشرفينــا لحضور " . $request->message2 . ' 🌺🌺 ';
-
-                    // $caption2 = 'تحرص الشركة على تقديم المساعدة للضيف حتى لا توجه اي صعوبات في دخول المناسبة تم ارسال الكود مره ثانية ,يرجى العلم ان الكود نفس الكود المرسل في السابق وليس كودا جديداً ';
-
-                    // $api=$client->sendChatMessage($to,$body);
-                    $api = $client->sendImageMessage($to,$url_image,$caption,$priority,$referenceId,$nocache);
-                    $api2 = $client->sendLocationMessage($to,$event->address,$event->lat,$event->long,$priority=0,$referenceId="SDK");
-
-                    // $qr_code_row = Qr_Code::where('event_user_id',$arr['id'])->latest()->first();
-
-                    // if($qr_code_row) {
-                    //     $image_link = url('qr_code/' . $qr_code_row->qr);
-                    //     // $api3 = $client->sendImageMessage($to,$image_link,$caption2,$priority,$referenceId,$nocache);
-                    // }
-
-                    if(! empty($api) && isset($api['sent']) && $api['sent'] == 'true'  && isset($api['message']) && $api['message'] == 'ok') {
-                        // dd('ok');
-                        info('error sending');
-                    } else {
-                        // dd('not ok',$api);
-                        $errors = $errors + 1;
-                    }
-
-
-
-
+                    // dd('not ok',$api);
+                    $errors = $errors + 1;
                 } 
             }
 

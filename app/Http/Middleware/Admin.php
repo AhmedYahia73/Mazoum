d6b5d202ca3e0172f5d7b8ed01f21d11c41ea7b2
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-06-09 20:36:33 +0300

    dsfdds

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -46,6 +46,8 @@ class Admin
                     "admin/excel-qr-sent-event-details/*",
                     "admin/event-messages",
                     "admin/scan_data",
+                    "admin/profile",
+                    "admin/update_profile",
                     "admin/open_event_family/*",
                     "admin/employee_attend",
                     "admin/attendance_list",

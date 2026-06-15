e5582965dc1a761128ef5117feba0ce5fef0573c
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-06-16 00:16:14 +0300

    gfhgrfh

--- database/migrations/2026_06_16_000812_add_column555_to_custom_event.php
+++ database/migrations/2026_06_16_000812_add_column555_to_custom_event.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumn55ToCustomEvent extends Migration
+class AddColumn555ToCustomEvent extends Migration
 {
     /**
      * Run the migrations.

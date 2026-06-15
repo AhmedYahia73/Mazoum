68c1864c0dad83a637b46cb898a552d384658a33
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-06-16 00:02:40 +0300

    fdgdf

--- app/Jobs/SendCustomEventPdfJob.php
+++ app/Jobs/SendCustomEventPdfJob.php
@@ -191,7 +191,7 @@ class SendCustomEventPdfJob implements ShouldQueue
             }
             </style>
             
-            <div style="position: absolute; bottom: 35mm; width: 100%; text-align: center;">
+            <div style="position: absolute; bottom: 35px; width: 100%; text-align: center;">
                 <a href="' . $confirm_link . '" style="text-decoration: none; display: inline-block;">
                     <img src="' . $buttonsImage . '" style="width: 130mm; height: auto; border: none; padding: 0; margin: 0;" />
                 </a>

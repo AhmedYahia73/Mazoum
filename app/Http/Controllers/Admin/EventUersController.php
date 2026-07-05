914199d813e9ea9003d016fa6dcc7d3ca062c2f2
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-07-06 00:18:04 +0300

    commit

--- app/Http/Controllers/Admin/EventUersController.php
+++ app/Http/Controllers/Admin/EventUersController.php
@@ -3598,7 +3598,8 @@ class EventUersController extends Controller
         $qr_code = Qr_Code::
         where("uu_id",$request->qr_id)
         ->first();
-        $Item = EventUsers::where('id', $qr_code->event_user_id)
+        $Item = EventUsers::
+        where('id', $qr_code->event_user_id)
         ->with("event")->first();
         $user_data = User::
         where("id", $Item->user_id)
@@ -3608,9 +3609,7 @@ class EventUersController extends Controller
             where("id", $Item?->event?->user_id)
             ->first();
         } 
-        $event = Events::where('id', $Item->event_id)
-        ->withTrashed()->first();
-        dd($event);
+        dd($Item);
         $available = $user_data->custom_invetaion - $user_data->send_custom_invetaion;
         if($request->users_count >= $available){
             return response()->json([

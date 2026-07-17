27b222f969471b0055f566293aa057531c6bd68e
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-07-17 16:51:25 +0300

    fdgvfd

--- app/Http/Controllers/Api/CustomEvent/UserController.php
+++ app/Http/Controllers/Api/CustomEvent/UserController.php
@@ -117,7 +117,12 @@ class UserController extends Controller
         where("id", auth()->user()->id)
         ->first();
         $available = $user->custom_invetaion - $user->send_custom_invetaion;
-        $user_custom_invetaion = $user->custom_invetaion - $request->custom_invetaion > 0 ? $request->custom_invetaion - $request->custom_invetaion : 0;
+        $user_custom_invetaion = $user->custom_invetaion - $request->custom_invetaion;
+        if($user_custom_invetaion < 0){
+            return response()->json([
+                "errors" => "أنت لا تملتلك كل هذه الدعوات"
+            ], 400);
+        }
         $user->custom_invetaion -=  $user_custom_invetaion;
         $user->balance -= $request->custom_invetaion;
         $user->save();

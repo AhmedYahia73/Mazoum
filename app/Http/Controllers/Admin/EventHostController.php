9ba18b7d2e4acf8421b36ffa76ad9ee1a068d50b
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-08-23 00:18:24 +0300

    fssd

--- app/Http/Controllers/Admin/EventHostController.php
+++ app/Http/Controllers/Admin/EventHostController.php
@@ -425,13 +425,13 @@ class EventHostController extends Controller
                 ->orWhere('mobile', 'like', "%$search%");
             });
         }
- 
-        $Item = $query->paginate(15)
-        ->through(function($item){
-            $item->balance = $item->available;
-            $item->available = $item->custom_invetaion;
-            return $item;
-        }); // عدد العناصر في الصفحة
+ $items = $query->paginate(15)->through(function ($item) {
+    // نجعل الـ available والـ balance يأخذان قيمة الـ custom_invetaion
+    $item->available = $item->custom_invetaion;
+    $item->balance = $item->available; 
+    
+    return $item;
+});// عدد العناصر في الصفحة
 
         return response()->json([
             'Item' => $Item,

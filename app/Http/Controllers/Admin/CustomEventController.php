deca5ac6ac5245dbf6ec8a383da396134db0ca21
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-08-24 23:01:52 +0300

    fdgdf

--- app/Http/Controllers/Admin/CustomEventController.php
+++ app/Http/Controllers/Admin/CustomEventController.php
@@ -2045,13 +2045,21 @@ class CustomEventController extends Controller
 
                   $day_name   = Carbon::parse($event->date)->locale('ar')->translatedFormat('l');
 
-                  $caption = $row->name . PHP_EOL . PHP_EOL .
-                    $event->title . PHP_EOL . PHP_EOL .
-                    "وذلك بمشيئة الله يوم " . $day_name ." الموافق" . PHP_EOL . PHP_EOL .
-                    $event->date . " 📆" . PHP_EOL . PHP_EOL .
-                    "⏱️الساعـة " . $event->time . " مساءاً" . PHP_EOL . PHP_EOL .
-                    "📍مكان الحفـل " . $event->address ;
+                //   $caption = $row->name . PHP_EOL . PHP_EOL .
+                //     $event->title . PHP_EOL . PHP_EOL .
+                //     "وذلك بمشيئة الله يوم " . $day_name ." الموافق" . PHP_EOL . PHP_EOL .
+                //     $event->date . " 📆" . PHP_EOL . PHP_EOL .
+                //     "⏱️الساعـة " . $event->time . " مساءاً" . PHP_EOL . PHP_EOL .
+                //     "📍مكان الحفـل " . $event->address ;
 
+                $caption =  $row->name . PHP_EOL . PHP_EOL .
+                    $event->title . PHP_EOL . PHP_EOL .
+                    // "وذلك بمشيئة الله تعالى يوم " . $day_name ." الموافق  📆 "  . $event->date
+                    // . PHP_EOL . PHP_EOL .
+                    // "وقت الاستقبال ⏱️الساعـة " . $event->time . " مساءاً" . PHP_EOL . PHP_EOL .
+                    // "📍مكان الحفـل " . $event->address  . PHP_EOL . PHP_EOL .
+                    "عدد الدعوات " . $row->users_count . PHP_EOL . PHP_EOL .
+                    "تم إرسـال هذه الرسالة من خـــلال تطبيق معزوم للدعوات الإلكترونية" ;
                 if($request->type == "pdf"){
                     \App\Jobs\SendCustomEventPdfJob::dispatch($row->id, $event->id, $ultramsg_token, $instance_id, $event->pdf_bottom, $caption);
                     // Mock API response since it's processed in background

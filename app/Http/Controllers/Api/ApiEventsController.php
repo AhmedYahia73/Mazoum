d40f5022177118d55e37261ca32932898428e481
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-09-06 00:05:01 +0300

    trytr

--- app/Http/Controllers/Api/ApiEventsController.php
+++ app/Http/Controllers/Api/ApiEventsController.php
@@ -1040,10 +1040,11 @@ class ApiEventsController extends Controller
             case 'failed_invitatios_users':
                 $query = EventUsers::where('event_id', $Item->id)
                 ->where('accept_count', 0)
-                ->where(function ($query) {
-                    $query->where('is_new_sent', '!=', 0)
-                        ->orWhere('status', '!=', 'hold')
-                        ->orWhereNotNull('is_sent');
+                ->where('status', "!=", 'not-attend')
+                ->where(function($query) { 
+                    $query->where('is_new_sent', "!=", 0)
+                    ->orWhere('status', "!=", 'hold')
+                    ->orWhereNotNull('is_sent'); 
                 });
                 break;
 

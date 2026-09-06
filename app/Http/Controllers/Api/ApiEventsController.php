8117e16742f1690153a8f62454b3a32b253791ef
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-09-07 01:22:45 +0300

    dhgdf

--- app/Http/Controllers/Api/ApiEventsController.php
+++ app/Http/Controllers/Api/ApiEventsController.php
@@ -1018,7 +1018,8 @@ class ApiEventsController extends Controller
                     ->where(function($q) use($user){
                         $q->where("user_id", $user->id)
                         ->orWhereNull("user_id");
-                    });
+                    })
+                    ->where("accept_count", ">", 0);
                 break;
 
             case 'scaned_qr_users':
